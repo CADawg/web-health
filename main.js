7f1f3f9f45cf3d82e87c7fc43b59a36a5e3b6745
hntr129 <lpostenyi@gmail.com>
2019-02-23 22:12:32 +0000

    Coordinate constants in

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 $(document).ready(function () {
-    
+
     const ayrshire = "S08000015";
+    const ayrshireCo = "[55.466888, -4.629981]";
     const borders = "S08000016";
-    const dumfries = "S08000017";   
+    const bordersCo = "[55.583244, -2.795106]";
+    const dumfries = "S08000017";
+    const dumfriesCo = "[55.059346, -3.625432]";
     const fife = "S08000018";
+    const fifeCo = "[56.117755, -3.185310]";
     const forth = "S08000019";
+    const forthCo = "[56.079339, -3.904547]";
     const grampian = "S08000020";
+    const grampianCo = "[57.146492, -2.098255]";
     const glasgow = "S08000021";
+    const glasgowCo = "[55.866305, -4.270132]";
     const highland = "S08000022";
+    const highlandCo = "[57.480787, -4.20073]";
     const lanarkshire = "S08000023";
+    const lanarkshireCo = "[55.839784, -3.997161]";
     const lothian = "S08000024";
+    const lothianCo = "[55.919304, -3.253777]";
     const orkney = "S08000025";
+    const orkneyCo = "[58.981869, -2.964337]";
     const shetland = "S08000026";
+    const shetlandCo = "[60.154613, -1.153110]";
     const tayside = "S08000027";
+    const taysideCo = "[56.464656, -3.031128]";
     const westisles = "S08000028";
+    const westislesCo = "[58.088539, -6.689270]";
 
 
     $.getJSON("https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=83b38aa6-73c0-4489-b344-11081e7aa0ed&q=male+2017/18+18-24", function(result) {
@@ -78,7 +92,7 @@ $(document).ready(function () {
                     break;
                 default:
                     break;
-                
+
             }
         //     //for each earthquake
         //     //get its coordinates
